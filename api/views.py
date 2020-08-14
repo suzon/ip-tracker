5868d3020cf9481836112942587ac4dedc99dca3
Suzon <engr.md.suzon@gmail.com>
2020-08-14 22:31:13 +0600

    IP tracker

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from ipware import get_client_ip
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
+
+@api_view(['GET', 'POST'])
+@renderer_classes([JSONRenderer])
+def ip_tracker(request):
+    if request.method == 'GET' or 'POST':
+        ip, is_routable = get_client_ip(request)
+        data = dict(
+            ip=ip,
+            public=is_routable
+        )
+        return Response(data, status=200)
